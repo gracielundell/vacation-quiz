2384b92550282e2411aef52a8233049a0f387032
Gracie Lundell <gracielundell@gmail.com>
2016-01-15 10:43:22 -0800

    Add answers to questions

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ $(document).ready(function(event) {
 // if total <= 5 ....
 // else if total <= 10 && total >= 6 ....
 
-
+// alert if total < 6: means not all q's were answered
 
   event.preventDefault();
 });

2dfaf0b9ae07dcc4719bfc6bfac1b083ba479f2f
Gracie Lundell <gracielundell@gmail.com>
2016-01-15 10:03:01 -0800

    Layout plan for js logic

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(event) {
+//add vars for diff id tags
+//var total = add up all answers
+
+
+//if/else branches based on where answer falls in a range
+// if total <= 5 ....
+// else if total <= 10 && total >= 6 ....
+
+
+
+  event.preventDefault();
+});
